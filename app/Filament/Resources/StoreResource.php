bfbf15fd107c97f6164d4df1dc5d59119d8efb90
AditKhalim <aditnur408@gmail.com>
2024-09-11 15:28:02 +0700

    first commit

---
+++ app/Filament/Resources/StoreResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Store;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Forms\Components\TextInput;
+use Filament\Resources\Resource;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\StoreResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\StoreResource\RelationManagers;
+
+class StoreResource extends Resource
+{
+    protected static ?string $model = Store::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-map-pin';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+
+                Forms\Components\TextArea::make('address')
+                ->required()
+                ->maxLength(1024),
+
+                Forms\Components\FileUpload::make('thumbnail')
+                ->image()
+                ->required(),
+
+                Forms\Components\Select::make('is_open')
+                ->options([
+                    true => 'Open',
+                    false => 'Closed',
+                ])
+                ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                ->searchable(),
+                Tables\Columns\ImageColumn::make('thumbnail'),
+                Tables\Columns\IconColumn::make('is_open')
+                    ->boolean()
+                    ->trueColor('success')
+                    ->falseColor('danger')
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->label('Open?'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStores::route('/'),
+            'create' => Pages\CreateStore::route('/create'),
+            'edit' => Pages\EditStore::route('/{record}/edit'),
+        ];
+    }
+}
